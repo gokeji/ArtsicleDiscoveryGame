219876f5e4a8bdda440b19fcee76de7f7a35bfe3
edacelen <edabingol@gmail.com>
2012-05-01 22:01:16 -0400

    art piece info added to facebook post

---
+++ image.php
@@ -0,0 +1,44 @@
+<?php
+
+// is cURL installed
+if (!function_exists('curl_init')){
+    die('Sorry cURL is not installed!');
+}
+
+$api_key = "lYfF8kAmPyRZ0Wr0G_9R";
+$api_url = "http://$api_key:artsicle@www.artsicle.com/api/v1";
+
+$art_perm = isset($_GET['art']) ? trim($_GET['art']) : 0;
+
+if (!$art_perm)
+{
+    echo "{'error': 'no image provided'}";
+    exit;
+}
+
+// create a new cURL resource handle
+$ch = curl_init();
+
+$url = $api_url."/art/$art_perm";
+
+// Set URL
+curl_setopt($ch, CURLOPT_URL, $url);
+
+// Include header in result
+curl_setopt($ch, CURLOPT_HEADER, 0);
+
+// Return the data
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+// 10 second timeout
+curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+
+// Fetch resource
+$output = curl_exec($ch);
+
+// Close the cURL
+curl_close($ch);
+
+echo $output;
+
+?>
